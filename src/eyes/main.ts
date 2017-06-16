0783d5fc7a7602db8476b637b6a30b01dc069535
Arne Vlaeminck <arne.vlaeminck@student.odisee.be>
2017-06-16 18:00:37 +0200

    Added eyes code

--- src/eyes/main.ts
+++ src/eyes/main.ts
@@ -1,4 +1,4 @@
-import * as Config from '../config/pinConfig';
+import Config from '../config/pinConfig';
 import {Led} from 'johnny-five';
 
 export class Eyes {
@@ -6,14 +6,26 @@ export class Eyes {
 	private rightLed: Led = new Led(Config['right-eye']);
 
 	constructor() {
+		console.log(Config['left-eye']);
 		this.leftLed = new Led(Config['left-eye']);
 		this.rightLed = new Led(Config['right-eye']);
+		this.turnOnEyes();
 	}
 
 	public wink = () => {
-		this.leftLed.on();
+		this.rightLed.off();
 		setTimeout(() => {
-			this.leftLed.off();
+			this.rightLed.on();
 		}, 500);
+	}
+
+	public turnOnEyes = () => {
+		this.leftLed.on();
+		this.rightLed.on();
+	}
+
+	public turnOffEyes = () => {
+			this.leftLed.off();
+			this.rightLed.off();
 	}
 }
